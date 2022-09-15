6edf34157773e447fcbebdea002e09edc901bb82
sandesh poudel <sandesh100poudel@gmail.com>
2022-09-15 13:34:03 +0545

    connecting mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+                               
+mongoose.connect("mongodb://localhost:27017/abc",
+{useNewUrlParser: true})
+.then(console.log("connected succesfully"))
+.catch((err)=>{
+    console.log(err);
+})
+
+
+
+
 
 app.listen(3000, ()=>{
     console.log("server is running");
-})
+});
